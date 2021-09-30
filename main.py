4be9552584341c2269ad2fb2134bf3722d7192c4
Nattharat Wiriyakulnan <nattharat@bandprotocol.com>
2021-09-30 17:29:26 +0700

    fix auth

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from pdgateway import create_app
 app = create_app(
     config={
         "API_KEY": environ.get("API_KEY"),
-        "BANDCHAIN_REST_ENDPOINT": "http://localhost:1317",
+        "BANDCHAIN_REST_ENDPOINT": environ.get("BANDCHAIN_REST_ENDPOINT"),
         "PREMIUM_ENDPOINT": environ.get("PREMIUM_ENDPOINT"),
     }
 )
@@ -21,23 +21,19 @@ app = create_app(
 # Feel free to implement this function in your own way.
 @app.route("/", methods=["GET"])
 def query():
-    # Get CoinGecko's IDs given by data source script
-    symbols = request.args.getlist("symbols")
-    params = {
-        "symbols": symbols,
-    }
-
     # This is an example of accessing app configuration,
     # which accesses value of API_KEY.
     api_key = current_app.config["API_KEY"]
     current_app.logger.debug("API_KEY:", api_key)
-    auth = HTTPBasicAuth(apiKey, api_key)
-
-    endpoint = current_app.config["PREMIUM_ENDPOINT"]
 
     # Send an HTTP request to CoinGecko to get coin prices
-    resp = requests.get(endpoint, params=params, auth=auth)
-
+    resp = requests.get(
+        current_app.config["PREMIUM_ENDPOINT"],
+        params={
+            "symbols": request.args.getlist("symbols"),
+        },
+        auth=HTTPBasicAuth("API Key", api_key),
+    )
     # The response may be directly returned without any modification
     # and allow the data source script to modify it before returning result to oracle script
     return resp.json(), resp.status_code
